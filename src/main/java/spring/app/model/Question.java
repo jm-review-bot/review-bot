6d4526846d1f80662cde561409cb4df162f29c8d
Anton Tavrel <atavrel@yandex.ru>
2020-03-24 19:28:39 +0300

    fix user table name, add some test data

--- src/main/java/spring/app/model/Question.java
+++ src/main/java/spring/app/model/Question.java
@@ -10,6 +10,7 @@ public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "question")
--- src/main/java/spring/app/model/StudentReviewAnswer.java
+++ src/main/java/spring/app/model/StudentReviewAnswer.java
@@ -9,6 +9,7 @@ public class StudentReviewAnswer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
