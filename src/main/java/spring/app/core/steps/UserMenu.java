c4f30619cf751f7ae0ddcda0cb50a243d1641922
Nikolay Klimov <krovoron@outlook.com>
2020-05-29 17:51:42 +0300

    выполнил 9 пункт: Влил актуальную версию дева (и не устранил конфликты - конфликтов с последней версией дева не имелось).

--- src/main/java/spring/app/core/steps/UserMenu.java
+++ src/main/java/spring/app/core/steps/UserMenu.java
@@ -144,12 +144,16 @@ public class UserMenu extends Step {
         } else if (command.contains("Принять")) { // (Принять ревью)
             nextStep = USER_TAKE_REVIEW_ADD_THEME;
             storageService.removeUserStorage(vkId, USER_MENU);
-        } else if (command.equals("/admin")
-                && context.getRole().isAdmin()) { // валидация что юзер имеет роль админ
-            nextStep = ADMIN_MENU;
-            storageService.removeUserStorage(vkId, USER_MENU);
+        } else if (command.equals("/admin")) {
+            if (context.getRole().isAdmin()) { // валидация что юзер имеет роль админ
+                nextStep = ADMIN_MENU;
+                storageService.removeUserStorage(vkId, USER_MENU);
+            } else {
+                throw new ProcessInputException("Недостаточно прав для выполнения команды!");
+            }
         } else { // любой другой ввод
             throw new ProcessInputException("Введена неверная команда...");
         }
+
     }
 }
