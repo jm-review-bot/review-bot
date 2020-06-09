56282bc26df84688b060979f6c09b1aaff60a0e9
Andrey <arkh.andr3w@gmail.com>
2020-06-09 18:45:05 +0300

    Изменил AdminRemoveUser: перенесл функционал обновления storageService в метод enter, добавил кнопки Да/Нет вместо нумерованого ответа пользователя

--- src/main/java/spring/app/core/steps/AdminRemoveUser.java
+++ src/main/java/spring/app/core/steps/AdminRemoveUser.java
@@ -18,12 +18,19 @@ import static spring.app.util.Keyboards.*;
 public class AdminRemoveUser extends Step {
 
     public AdminRemoveUser() {
-        super("", "");
+        super("", YES_NO_KB);
     }
 
     @Override
     public void enter(BotContext context) {
+        Integer vkId = context.getVkId();
+        StorageService storageService = context.getStorageService();
+        String selectedUserId = storageService.getUserStorage(vkId, ADMIN_USERS_LIST).get(0);
+        User selectedUser = context.getUserService().getUserById(Long.parseLong(selectedUserId));
 
+        String userInfo = new StringBuilder().append(selectedUser.getFirstName()).append(" ").append(selectedUser.getLastName())
+                .append(" (https://vk.com/id").append(selectedUser.getVkId()).append(").\n").toString();
+        storageService.updateUserStorage(vkId, ADMIN_REMOVE_USER, Arrays.asList(userInfo));
     }
 
     @Override
@@ -31,52 +38,38 @@ public class AdminRemoveUser extends Step {
         String currentInput = context.getInput();
         StorageService storageService = context.getStorageService();
         Integer vkId = context.getVkId();
-
         String wordInput = StringParser.toWordsArray(currentInput)[0];
-        if (StringParser.isNumeric(wordInput)) {
-            Integer selectedNumber = Integer.parseInt(wordInput);
-            switch (selectedNumber) {
-                case 1:
-                    //Удаляем.
-                    String selectedUserId = storageService.getUserStorage(vkId, ADMIN_USERS_LIST).get(0);
-                    storageService.clearUsersOfStorage(context.getUserService().getUserById(Long.parseLong(selectedUserId)).getVkId());
-                    context.getUserService()
-                            .deleteUserById(Long.parseLong(selectedUserId));
-                    //перекинем инфу с удаления на список юзеров. Для унификации. Данная инфа будет использована при возвращении
-                    String userInfo = storageService.getUserStorage(vkId, ADMIN_REMOVE_USER).get(0);
-                    storageService.removeUserStorage(vkId, ADMIN_REMOVE_USER);
-                    storageService.updateUserStorage(vkId, ADMIN_USERS_LIST, Arrays.asList(userInfo));
-                    sendUserToNextStep(context, ADMIN_USERS_LIST);
-                    break;
-                case 2:
-                    //Возвращаемся назад
-                    storageService.removeUserStorage(vkId, ADMIN_REMOVE_USER);
-                    //Очищаем список ролей, раз не хотим ничего сообщать в том шаге
-                    storageService.removeUserStorage(vkId, ADMIN_USERS_LIST);
-                    sendUserToNextStep(context, ADMIN_USERS_LIST);
-                    break;
-                default:
-                    throw new ProcessInputException("Введено неподходящее число");
-            }
-        } else {
-            throw new NoNumbersEnteredException("Введена неверная команда. Введите 1 для удаления или 2 для возврата");
+
+        if (wordInput.equals("да")) {
+            String selectedUserId = storageService.getUserStorage(vkId, ADMIN_USERS_LIST).get(0);
+            storageService.clearUsersOfStorage(context.getUserService().getUserById(Long.parseLong(selectedUserId)).getVkId());
+            context.getUserService()
+                    .deleteUserById(Long.parseLong(selectedUserId));
+            //перекинем инфу с удаления на список юзеров. Для унификации. Данная инфа будет использована при возвращении
+            String userInfo = storageService.getUserStorage(vkId, ADMIN_REMOVE_USER).get(0);
+            storageService.removeUserStorage(vkId, ADMIN_REMOVE_USER);
+            storageService.updateUserStorage(vkId, ADMIN_USERS_LIST, Arrays.asList(userInfo));
+            sendUserToNextStep(context, ADMIN_USERS_LIST);
+        }
+        else if (wordInput.equals("нет")) {
+            //Возвращаемся назад
+            storageService.removeUserStorage(vkId, ADMIN_REMOVE_USER);
+            //Очищаем список ролей, раз не хотим ничего сообщать в том шаге
+            storageService.removeUserStorage(vkId, ADMIN_USERS_LIST);
+            sendUserToNextStep(context, ADMIN_USERS_LIST);
+        }
+        else {
+            throw new NoNumbersEnteredException("Введена неверная команда. Нажмите \"Да\" для удаления или \"Нет\" для возврата.");
         }
     }
 
     @Override
     public String getDynamicText(BotContext context) {
-        Integer vkId = context.getVkId();
         StorageService storageService = context.getStorageService();
-        String selectedUserId = storageService.getUserStorage(vkId, ADMIN_USERS_LIST).get(0);
-        User selectedUser = context.getUserService().getUserById(Long.parseLong(selectedUserId));
-        String userInfo = new StringBuilder().append(selectedUser.getFirstName()).append(" ").append(selectedUser.getLastName())
-                .append(" (https://vk.com/id").append(selectedUser.getVkId()).append(").\n").toString();
+        String userInfo = storageService.getUserStorage(context.getVkId(), ADMIN_REMOVE_USER).get(0);
         StringBuilder confirmMessage = new StringBuilder("Студент ");
         confirmMessage.append(userInfo)
-                .append("Вы точно хотите удалить данного студента?\n")
-                .append("[1] подтвердить\n")
-                .append("[2] отменить\n");
-        storageService.updateUserStorage(vkId, ADMIN_REMOVE_USER, Arrays.asList(userInfo));
+                .append("Вы точно хотите удалить данного студента? (Да/Нет)\n");
         return confirmMessage.toString();
     }
 
