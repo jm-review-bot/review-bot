586fd150464a36d9286446023d94e8d3cf202005
Nikolay Klimov <krovoron@outlook.com>
2020-06-10 19:31:33 +0300

    Исправлена проблема в UserTakeReviewAddDate (указанная Akira Rokudo в трэлло в карточке "Набор классов для записи на ревью ревьюером + запись студентом(UserTakeReviewAddTheme и UserPassReviewAddTheme соответственно - 5 классов)" в комментарии в 08.06.2020 в 18:56). А именно - лишняя проверка на то, что "осталось не менее 60 минут до выбранной даты".

--- src/main/java/spring/app/core/steps/UserTakeReviewAddDate.java
+++ src/main/java/spring/app/core/steps/UserTakeReviewAddDate.java
@@ -53,53 +53,51 @@ public class UserTakeReviewAddDate extends Step {
             } catch (NoDataEnteredException e) {
                 throw new ProcessInputException("Некорректный ввод данных...\n\n Пример корректного ответа 02.06.2020 17:30");
             }
-            if (plannedStartReviewTime.isAfter(LocalDateTime.now().plusMinutes(timeLimitBeforeReview))) {
-                if (plannedStartReviewTime.isAfter(LocalDateTime.now())) {
-                    List<Review> conflictReviews = context.getReviewService().getOpenReviewsByReviewerVkId(vkId, plannedStartReviewTime, reviewDuration);
-                    List<Review> conflictStudentReviews = context.getReviewService().getOpenReviewsByStudentVkId(vkId, plannedStartReviewTime, reviewDuration);
-                    if (!conflictReviews.isEmpty()) {
-                        Review conflictReview = conflictReviews.get(0);
-                        StringBuilder conflictExceptionMessage = new StringBuilder();
-                        conflictExceptionMessage.append("Новое ревью пересекается с другим ревью, которое ты планируешь провести.")
-                                .append(String.format("\n\nОбрати внимание, что длительность ревью %d минут", (reviewDuration + 1)))
-                                .append(String.format("\n\nПересечение с ревью:\nТема: %s", context.getThemeService().getThemeByReviewId(conflictReview.getId()).getTitle()))
-                                .append(String.format("\nДата начала ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate())))
-                                .append(String.format("\nДата окончания ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate().plusMinutes(reviewDuration + 1))))
-                                .append("\n\nПовтори ввод или вернись назад к выбору темы ревью");
-                        throw new ProcessInputException(conflictExceptionMessage.toString());
-                    } else if (!conflictStudentReviews.isEmpty()) {
-                        Review conflictReview = conflictStudentReviews.get(0);
-                        StringBuilder conflictExceptionMessage = new StringBuilder();
-                        conflictExceptionMessage.append("Новое ревью пересекается с другим ревью, в котором ты участвуешь.")
-                                .append(String.format("\n\nОбрати внимание, что длительность ревью %d минут", (reviewDuration + 1)))
-                                .append(String.format("\n\nПересечение с ревью:\nТема: %s", context.getThemeService().getThemeByReviewId(conflictReview.getId()).getTitle()))
-                                .append(String.format("\nДата начала ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate())))
-                                .append(String.format("\nДата окончания ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate().plusMinutes(reviewDuration + 1))))
-                                .append("\n\nПовтори ввод или вернись назад к выбору темы ревью");
-                        throw new ProcessInputException(conflictExceptionMessage.toString());
-                    } else {
-                        //все хорошо с валидацией, создаем ревью.
-                        User user = context.getUserService().getByVkId(vkId);
-                        Long themeId = (Long.parseLong(storageService.getUserStorage(vkId, USER_TAKE_REVIEW_ADD_THEME).get(0)));
-                        Theme theme = context.getThemeService().getThemeById(themeId);
-                        context.getReviewService().addReview(new Review(user, theme, true, plannedStartReviewTime));
-                        storageService.removeUserStorage(vkId, USER_TAKE_REVIEW_ADD_THEME);
-                        storageService.removeUserStorage(vkId, USER_TAKE_REVIEW_ADD_DATE);
-                        sendUserToNextStep(context, USER_MENU);
-                        String textForSend = String.format(String.format("Супер! Твоё ревью добавлено в сетку расписания, в день и время когда оно наступит нажми на кнопку \"Начать ревью\"\n\nВаше ревью '%%s' %%s было успешно добавлено в сетку расписания\n\n"), theme.getTitle(), userInput);
-                        //и шлем сообщение.А вот и не шлем.Мы не знаем какие кнопки отобразить пользователю.Так что
-                        //отправлять сообщения таким образом мы можем всем кроме себя.Для себя пользуемся хаком
-                        //с хранилищем -
-                        //        кладем сообщения на шаг, на который планируем придти
-                        context.getVkService().sendMessage(textForSend, this.getComposeKeyboard(context), vkId);
-                        context.getStorageService().updateUserStorage(vkId, USER_MENU, Arrays.asList(textForSend));
-                    }
-                } else if (plannedStartReviewTime.isBefore(LocalDateTime.now())) {
-                    throw new ProcessInputException("Время принятия нового ревью не может быть в прошлом :)\n Повтори ввод или вернись назад к выбору темы ревью");
-                } else if (plannedStartReviewTime.isBefore(LocalDateTime.now().plusMinutes(timeLimitBeforeReview))) {
-                    throw new ProcessInputException(String.format("Ты можешь объявить о готовности принять, ревью не ранее, чем за %d минут до его начала\n" +
-                            "Повтори ввод или вернись назад к выбору темы ревью", (timeLimitBeforeReview + 1)));
+            if (plannedStartReviewTime.isAfter(LocalDateTime.now())) {
+                List<Review> conflictReviews = context.getReviewService().getOpenReviewsByReviewerVkId(vkId, plannedStartReviewTime, reviewDuration);
+                List<Review> conflictStudentReviews = context.getReviewService().getOpenReviewsByStudentVkId(vkId, plannedStartReviewTime, reviewDuration);
+                if (!conflictReviews.isEmpty()) {
+                    Review conflictReview = conflictReviews.get(0);
+                    StringBuilder conflictExceptionMessage = new StringBuilder();
+                    conflictExceptionMessage.append("Новое ревью пересекается с другим ревью, которое ты планируешь провести.")
+                            .append(String.format("\n\nОбрати внимание, что длительность ревью %d минут", (reviewDuration + 1)))
+                            .append(String.format("\n\nПересечение с ревью:\nТема: %s", context.getThemeService().getThemeByReviewId(conflictReview.getId()).getTitle()))
+                            .append(String.format("\nДата начала ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate())))
+                            .append(String.format("\nДата окончания ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate().plusMinutes(reviewDuration + 1))))
+                            .append("\n\nПовтори ввод или вернись назад к выбору темы ревью");
+                    throw new ProcessInputException(conflictExceptionMessage.toString());
+                } else if (!conflictStudentReviews.isEmpty()) {
+                    Review conflictReview = conflictStudentReviews.get(0);
+                    StringBuilder conflictExceptionMessage = new StringBuilder();
+                    conflictExceptionMessage.append("Новое ревью пересекается с другим ревью, в котором ты участвуешь.")
+                            .append(String.format("\n\nОбрати внимание, что длительность ревью %d минут", (reviewDuration + 1)))
+                            .append(String.format("\n\nПересечение с ревью:\nТема: %s", context.getThemeService().getThemeByReviewId(conflictReview.getId()).getTitle()))
+                            .append(String.format("\nДата начала ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate())))
+                            .append(String.format("\nДата окончания ревью: %s", StringParser.localDateTimeToString(conflictReview.getDate().plusMinutes(reviewDuration + 1))))
+                            .append("\n\nПовтори ввод или вернись назад к выбору темы ревью");
+                    throw new ProcessInputException(conflictExceptionMessage.toString());
+                } else {
+                    //все хорошо с валидацией, создаем ревью.
+                    User user = context.getUserService().getByVkId(vkId);
+                    Long themeId = (Long.parseLong(storageService.getUserStorage(vkId, USER_TAKE_REVIEW_ADD_THEME).get(0)));
+                    Theme theme = context.getThemeService().getThemeById(themeId);
+                    context.getReviewService().addReview(new Review(user, theme, true, plannedStartReviewTime));
+                    storageService.removeUserStorage(vkId, USER_TAKE_REVIEW_ADD_THEME);
+                    storageService.removeUserStorage(vkId, USER_TAKE_REVIEW_ADD_DATE);
+                    sendUserToNextStep(context, USER_MENU);
+                    String textForSend = String.format(String.format("Супер! Твоё ревью добавлено в сетку расписания, в день и время когда оно наступит нажми на кнопку \"Начать ревью\"\n\nВаше ревью '%%s' %%s было успешно добавлено в сетку расписания\n\n"), theme.getTitle(), userInput);
+                    //и шлем сообщение.А вот и не шлем.Мы не знаем какие кнопки отобразить пользователю.Так что
+                    //отправлять сообщения таким образом мы можем всем кроме себя.Для себя пользуемся хаком
+                    //с хранилищем -
+                    //        кладем сообщения на шаг, на который планируем придти
+                    context.getVkService().sendMessage(textForSend, this.getComposeKeyboard(context), vkId);
+                    context.getStorageService().updateUserStorage(vkId, USER_MENU, Arrays.asList(textForSend));
                 }
+            } else if (plannedStartReviewTime.isBefore(LocalDateTime.now())) {
+                throw new ProcessInputException("Время принятия нового ревью не может быть в прошлом :)\n Повтори ввод или вернись назад к выбору темы ревью");
+            } else if (plannedStartReviewTime.isBefore(LocalDateTime.now().plusMinutes(timeLimitBeforeReview))) {
+                throw new ProcessInputException(String.format("Ты можешь объявить о готовности принять, ревью не ранее, чем за %d минут до его начала\n" +
+                        "Повтори ввод или вернись назад к выбору темы ревью", (timeLimitBeforeReview + 1)));
             }
         }
     }
