c94017acae605b6d529d93b55fa2fb6a4ad0438f
Nikolay Klimov <krovoron@outlook.com>
2020-06-12 12:27:46 +0300

    переделал UserTakeReviewAddTheme в соответствии с требованием "Убрать поле themes - хоть и маловероятно, но это потенциальная дырка. Смотри решение в AdminSetThemeAddedUser"

--- src/main/java/spring/app/core/steps/UserTakeReviewAddTheme.java
+++ src/main/java/spring/app/core/steps/UserTakeReviewAddTheme.java
@@ -6,10 +6,7 @@ import spring.app.exceptions.ProcessInputException;
 import spring.app.model.Theme;
 import spring.app.service.abstraction.StorageService;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static java.util.stream.Collectors.toList;
 import static spring.app.core.StepSelector.*;
@@ -22,30 +19,38 @@ public class UserTakeReviewAddTheme extends Step {
         super("", DEF_BACK_KB);
     }
 
-    private Map<Integer, Theme> themes = new HashMap<>();
-
     @Override
     public void enter(BotContext context) {
+        StringBuilder themeList = new StringBuilder("Выбери тему, которую хочешь принять, в качестве ответа пришли цифру (номер темы):\n" +
+                "Ты можешь принимать ревью только по тем темам, которые успешно сдал.\n\n");;
+        List<String> listTheme = new ArrayList<>();
+        List<Theme> themes = context.getThemeService().getAllThemes();
+        for (Theme position : themes) {
+            themeList.append(String.format("[%d] %s\n", position.getPosition(), position.getTitle()));
+        }
+        themeList.append("\nИли нажмите на кнопку \"Назад\" для возврата к предыдущему меню.");
+        listTheme.add(themeList.toString());
+        context.getStorageService().updateUserStorage(context.getVkId(), USER_TAKE_REVIEW_ADD_THEME, listTheme);
     }
 
     @Override
     public void processInput(BotContext context) throws ProcessInputException {
         StorageService storageService = context.getStorageService();
         String userInput = context.getInput();
+        List<Theme> themes = context.getThemeService().getAllThemes();
         Integer vkId = context.getVkId();
-        List<String> themePositionsList = themes.keySet().stream()
+        List<String> themePositionsList = themes.stream()
+                .map(Theme::getPosition)
                 .map(Object::toString)
                 .collect(toList());
         if (themePositionsList.contains(userInput)) {
-            // вытаскиваем themeId по позиции, позиция соответствует пользовательскому вводу
-            String themeId = themes.get(Integer.parseInt(userInput)).getId().toString();
+            // вытаскиваем тему по позиции, позиция соответствует пользовательскому вводу
+            Theme selectedTheme = context.getThemeService().getByPosition(Integer.parseInt(userInput));
             // проверяем, что сдали ревью по теме, которую хотим принять
-            List<String> passedThemesIds = context.getThemeService().getPassedThemesByUser(vkId).stream()
-                    .map(theme -> theme.getId().toString())
-                    .collect(toList());
-            if (passedThemesIds.contains(themeId)) {
+            List<Theme> passedThemesIds = context.getThemeService().getPassedThemesByUser(vkId);
+            if (passedThemesIds.contains(selectedTheme)) {
                 // складываем в хранилище для использования в следующих шагах
-                storageService.updateUserStorage(vkId, USER_TAKE_REVIEW_ADD_THEME, Arrays.asList(themeId));
+                storageService.updateUserStorage(vkId, USER_TAKE_REVIEW_ADD_THEME, Arrays.asList(selectedTheme.getId().toString()));
                 sendUserToNextStep(context, USER_TAKE_REVIEW_ADD_DATE);
             } else {
                 throw new ProcessInputException("Ты пока не можешь принять эту тему, потому что не сдал по ней ревью.\n\n" +
@@ -66,20 +71,15 @@ public class UserTakeReviewAddTheme extends Step {
                 storageService.removeUserStorage(vkId, USER_TAKE_REVIEW_ADD_DATE);
             }
         } else {
-            throw new ProcessInputException("Введена неверная команда...\n\n Введи цифру, соответствующую теме рьвью или нажми на кнопку \"Назад\" для возврата в главное меню");
+            throw new ProcessInputException("Введена неверная команда...\n\n" +
+                    "Введи цифру, соответствующую теме рьвью или нажми на кнопку \"Назад\" для возврата в главное меню");
         }
     }
 
     @Override
     public String getDynamicText(BotContext context) {
-        context.getThemeService().getAllThemes().forEach(theme -> themes.putIfAbsent(theme.getPosition(), theme));
-        StringBuilder themeList = new StringBuilder("Выбери тему, которую хочешь принять, в качестве ответа пришли цифру (номер темы):\n ");
-        themeList.append("Ты можешь принимать ревью только по тем темам, которые успешно сдал.\n\n");
-        for (Integer position : themes.keySet()) {
-            themeList.append(String.format("[%d] %s\n", position, themes.get(position).getTitle()));
-        }
-        themeList.append("\nИли нажмите на кнопку \"Назад\" для возврата к предыдущему меню.");
-        return themeList.toString();
+        List<String> themeList = context.getStorageService().getUserStorage(context.getVkId(), USER_TAKE_REVIEW_ADD_THEME);
+        return themeList.get(0);
     }
 
     @Override
