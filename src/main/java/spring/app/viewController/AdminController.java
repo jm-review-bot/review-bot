591a9b52089977120182eb20fa15d97d9be15a26
drbah <dr.bahahza@yandex.ru>
2020-07-06 17:13:34 +0300

    Внес правки по коментариям Доработал верстку

--- src/main/java/spring/app/viewController/AdminController.java
+++ src/main/java/spring/app/viewController/AdminController.java
@@ -1,11 +1,14 @@
 package spring.app.viewController;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ThemeView {
-    @RequestMapping(value = "/admin/theme")
+@RequestMapping("/admin/theme")
+public class AdminController {
+
+    @GetMapping(value = "")
     public String index() {
         return "theme";
     }
