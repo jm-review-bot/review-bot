b2fefd9d32d5ffb870c4faf4a794e9586ecba32e
Dmitry Shepelev <dshepelev15@list.ru>
2020-07-06 22:49:37 +0300

    Поправил согласно комментариям

--- src/main/java/spring/app/controlller/QuestionRestController.java
+++ src/main/java/spring/app/controlller/QuestionRestController.java
@@ -7,23 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.app.service.abstraction.QuestionService;
 
-@RequestMapping("/admin/theme/{themeId}/question/{questionId}/position/")
+@RequestMapping("/admin/theme/{themeId}/question")
 @RestController
-public class QuestionPositionController {
+public class QuestionRestController {
 
     private QuestionService questionService;
 
-    public QuestionPositionController(QuestionService questionService) {
+    public QuestionRestController(QuestionService questionService) {
         this.questionService = questionService;
     }
 
-    @PatchMapping("up")
+    @PatchMapping("/{questionId}/position/up")
     public ResponseEntity<?> moveThemeQuestionPositionUp(@PathVariable Long themeId, @PathVariable Long questionId) {
         boolean isChanged = questionService.changeQuestionPositionByThemeIdAndQuestionIdAndPositionShift(themeId, questionId, -1);
         return isChanged ? ResponseEntity.ok("Вопрос перемещён на позицию выше") : ResponseEntity.badRequest().build();
     }
 
-    @PatchMapping("down")
+    @PatchMapping("/{questionId}/position/down")
     public ResponseEntity<?> moveThemeQuestionPositionDown(@PathVariable Long themeId, @PathVariable Long questionId) {
         boolean isChanged = questionService.changeQuestionPositionByThemeIdAndQuestionIdAndPositionShift(themeId, questionId, 1);
         return isChanged ? ResponseEntity.ok("Вопрос перемещён на позицию ниже") : ResponseEntity.badRequest().build();
