1f45a059840d3a8ec59e577e6feab403a4837e5d
Admin <mkuzivanov@gmail.com>
2020-07-11 00:57:00 +0300

    Теперь список всех тем формируется по возрастанию порядкового номера

--- src/main/resources/static/js/themePageService.js
+++ src/main/resources/static/js/themePageService.js
@@ -18,44 +18,52 @@ function getAllThemesDto() {
 
 function buildThemesAccordion(allThemesDto) {
     let htmlContent = ''
-    $(allThemesDto).each((i, theme) => {
+    /*
+    * Для того, чтобы темы отображались в порядке возрастания номера позиции,
+    * запускает цикл по количеству тем и из всего массива тем выбирается лишь та,
+    * у которой номер позиции совпадает с текущей итерацией цикла
+    * */
+    for (let i = 1; i <= allThemesDto.length; i++) {
+        let theme = $(allThemesDto).filter(index => {
+            return allThemesDto[index].position == i
+        })[0]
         let themeHtmlAccordion = `
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="mb-0">
-                        <div class="row">
-                            <div class="col-10 theme-expand" role="button" data-toggle="collapse" data-target="#theme-${theme.id}" aria-expanded="true" aria-controls="theme-${theme.id}" data-id="${theme.id}">
-                                ${theme.title}
-                            </div>
-                            <div class="col-2 text-right">
-                                <button class="move-down-theme btn btn-link" type="button" data-id="${theme.id}">
-                                    <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
-                                </button>
-                                <button class="move-up-theme btn btn-link" type="button" data-id="${theme.id}">
-                                    <span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span>
-                                </button>
-                                <button class="edit-theme btn btn-link" type="button" data-id="${theme.id}">
-                                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                                </button>
-                            </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="mb-0">
+                    <div class="row">
+                        <div class="col-10 theme-expand" role="button" data-toggle="collapse" data-target="#theme-${theme.id}" aria-expanded="true" aria-controls="theme-${theme.id}" data-id="${theme.id}">
+                            ${theme.title}
                         </div>
-                    </h4>
+                        <div class="col-2 text-right">
+                            <button class="move-down-theme btn btn-link" type="button" data-id="${theme.id}">
+                                <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
+                            </button>
+                            <button class="move-up-theme btn btn-link" type="button" data-id="${theme.id}">
+                                <span class="glyphicon glyphicon-menu-up" aria-hidden="true"></span>
+                            </button>
+                            <button class="edit-theme btn btn-link" type="button" data-id="${theme.id}">
+                                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                            </button>
+                        </div>
+                    </div>
+                </h4>
+            </div>
+            <div id="theme-${theme.id}" class="collapse" aria-labelledby="card-header-theme-${theme.id}">
+                <div class="card-body">
+                    <div id="card-theme-${theme.id}" class="card">
+                    <!--Здесь располагаются вопросы темы-->
+                    </div>
                 </div>
-                <div id="theme-${theme.id}" class="collapse" aria-labelledby="card-header-theme-${theme.id}">
-                    <div class="card-body">
-                        <div id="card-theme-${theme.id}" class="card">
-                        <!--Здесь располагаются вопросы темы-->
-                        </div>
-                    </div>
-                    <div>
-                        <button type="button" class="add-question-to-theme btn btn-outline-primary btn-lg btn-block" data-id="${theme.id}">
-                            Добавить вопрос
-                        </button>
-                    </div>
+                <div>
+                    <button type="button" class="add-question-to-theme btn btn-outline-primary btn-lg btn-block" data-id="${theme.id}">
+                        Добавить вопрос
+                    </button>
                 </div>
             </div>
-        `
+        </div>
+    `
         htmlContent += themeHtmlAccordion;
-    })
+    }
     $('#theme-accordion').html(htmlContent)
 }
