3f7170a572ca30bf2c3659a3525f11117b30f250
Nikolay Klimov <krovoron@outlook.com>
2020-07-13 12:52:55 +0300

    Убрал лишнюю конструкцию $(function () {     ...... }) Добавил закрытие модального окна добавления темы при удачном завершении POST-запроса на сохранение темы. Добавил вывод сообщения "ОШИБКА: POST-запрос добавления темы неудачно завершился" в случае неудачного завершения POST-запроса на сохранение темы.

--- src/main/resources/static/js/addTheme.js
+++ src/main/resources/static/js/addTheme.js
@@ -1,20 +1,22 @@
-$(function () {
-    $('.add-new-theme').onclick(function () {
-            let value = document.getElementById('idInputForNameOfTheNewTheme').value;
-            let json = {
-                title:value,
-                criticalWeight:8,
-                reviewPoint:4
-            }
-            $.ajax({
-                url: "/api/admin/theme",
-                type: "POST",
-                success: function() {
-                    buildThemesAccordion(getAllThemesDto());
-                },
-                contentType: "application/json",
-                data: JSON.stringify(json)
-            });
+$('.add-new-theme').click(function () {
+        let value = $('#idInputForNameOfTheNewTheme').val();
+        let json = {
+            title:value,
+            criticalWeight:8,
+            reviewPoint:4
         }
-    )
-})
+        $.ajax({
+            url: "/api/admin/theme",
+            type: "POST",
+            success: function() {
+                buildThemesAccordion(getAllThemesDto());
+                $('#containerForAddingTheme').modal('hide');
+            },
+            error: function() {
+                alert("ОШИБКА: POST-запрос добавления темы неудачно завершился.");
+            },
+            contentType: "application/json",
+            data: JSON.stringify(json)
+        });
+    }
+)
