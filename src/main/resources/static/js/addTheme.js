31e31917532e4f75159cf35a63ddb5b7e399aa59
Nikolay Klimov <krovoron@outlook.com>
2020-07-15 18:52:49 +0300

    Скорректировал addTheme.css в соответствии с замечаниями Ивана на гитхабе. В theme.html добавил в модальное окно добавления новой темы поля ввода критического веса темы и количество очков за сдачу темы. В reviewBot.css добавил стили для задания адекватной ширины полей ввода в модальном окне добавления новой темы.

--- src/main/resources/static/js/addTheme.js
+++ src/main/resources/static/js/addTheme.js
@@ -1,22 +1,24 @@
 $('.add-new-theme').click(function () {
-        let value = $('#idInputForNameOfTheNewTheme').val();
+        let title = $('#idInputForNameOfTheNewTheme').val();
+        let criticalWeight = $('#idInputForCriticalWeightOfTheNewTheme').val();
+        let reviewPoint = $('#idInputForReviewPointOfTheNewTheme').val();
         let json = {
-            title:value,
-            criticalWeight:8,
-            reviewPoint:4
+            title:title,
+            criticalWeight:criticalWeight,
+            reviewPoint:reviewPoint
         }
         $.ajax({
             url: "/api/admin/theme",
             type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(json),
             success: function() {
                 buildThemesAccordion(getAllThemesDto());
                 $('#containerForAddingTheme').modal('hide');
             },
             error: function() {
                 alert("ОШИБКА: POST-запрос добавления темы неудачно завершился.");
-            },
-            contentType: "application/json",
-            data: JSON.stringify(json)
+            }
         });
     }
 )
