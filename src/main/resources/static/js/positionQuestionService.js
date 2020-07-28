6e7ee9d0ff12ad0e3ff8244ca3005614712734a4
Admin <mkuzivanov@gmail.com>
2020-07-28 23:49:56 +0300

    FRONT: Изменение позиции вопроса

--- src/main/resources/static/js/positionQuestionService.js
+++ src/main/resources/static/js/positionQuestionService.js
@@ -1,4 +1,14 @@
-// Изменение позиции вопроса на одну вниз
+/*
+* По нажатию на "Переместить вопрос ниже" производится запрос на бэкенд
+* для выполнения соответсвующей операции. При успешном выполнении список
+* вопросов текущей темы должен обновиться
+*
+* move-down-question - кнопка, по которой должен перемещаться
+* вопрос на подну позицию ниже.
+*
+* this.dataset.idtheme - ID темы, который заложен в кнопку.
+* this.dataset.idquestion - ID вопроса, который заложен в кнопку.
+* */
 $(document).on('click', '.move-down-question', function () {
     let themeId = this.dataset.idtheme
     let questionId = this.dataset.idquestion
@@ -14,7 +24,17 @@ $(document).on('click', '.move-down-question', function () {
     })
 })
 
-// Изменение позиции вопроса на одну вверх
+/*
+* По нажатию на "Переместить вопрос выше" производится запрос на бэкенд
+* для выполнения соответсвующей операции. При успешном выполнении список
+* вопросов текущей темы должен обновиться
+*
+* move-up-question - кнопка, по которой должен перемещаться
+* вопрос на подну позицию ниже.
+*
+* this.dataset.idtheme - ID темы, который заложен в кнопку.
+* this.dataset.idquestion - ID вопроса, который заложен в кнопку.
+* */
 $(document).on('click', '.move-up-question', function () {
     let themeId = this.dataset.idtheme
     let questionId = this.dataset.idquestion
