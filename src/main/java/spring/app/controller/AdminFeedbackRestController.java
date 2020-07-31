bace1472ce43174727ffcfddfbfd6fceb1c85fc7
Mikhail Kuzivanov <mkuzivanov@gmail.com>
2020-07-31 10:13:08 +0300

    Security

--- src/main/java/spring/app/controller/AdminFeedbackRestController.java
+++ src/main/java/spring/app/controller/AdminFeedbackRestController.java
@@ -1,6 +1,7 @@
 package spring.app.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Validated
 @RestController
+@PreAuthorize("hasAuthority('ADMIN')")
 @RequestMapping("/api/admin/feedback")
 public class AdminFeedbackRestController {
     private FeedbackService feedbackService;
