8c3c25df2d6489b5d9e18ff8ad53aa92cfb0d52f
JeRiF74 <jerif174@gmail.com>
2020-08-06 16:08:59 +0300

    commit 1.2

--- src/main/java/spring/app/controller/AdminReviewerRestController.java
+++ src/main/java/spring/app/controller/AdminReviewerRestController.java
@@ -1,11 +1,15 @@
 package spring.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import spring.app.dto.ReviewerDto;
 import spring.app.groups.UpdateGroup;
+import spring.app.model.User;
 import spring.app.service.abstraction.UserService;
 
 import javax.validation.Valid;
@@ -15,6 +19,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/admin/theme")
 public class AdminReviewerRestController {
+
+    private final static Logger log = LoggerFactory.getLogger(AdminReviewerRestController.class);
 
     private UserService userService;
 
@@ -30,22 +36,29 @@ public class AdminReviewerRestController {
 
     @GetMapping("/{themeId}/reviewer/notInThisTheme")
     public ResponseEntity<List<ReviewerDto>> getAllReviewersNotInThisTheme (@PathVariable long themeId) {
-       List<ReviewerDto> examiners = userService.getExaminersInNotThisTheme(themeId);
-       return ResponseEntity.ok(examiners);
+        List<ReviewerDto> examiners = userService.getExaminersInNotThisTheme(themeId);
+        return ResponseEntity.ok(examiners);
     }
 
     @Validated(UpdateGroup.class)
     @PostMapping("/{themeId}/reviewer")
     public ResponseEntity<ReviewerDto> create (@PathVariable long themeId ,
                                                @RequestBody @Valid ReviewerDto reviewerDto) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         userService.addNewReviewer(themeId , reviewerDto.getId());
+        log.info("Admin(vkId={}) добавил ревьювера (Reviewer=()) в тему (Theme=())" ,
+                user.getVkId() , reviewerDto.getId()  + "," + reviewerDto.getFirstName() + "," + reviewerDto.getLastName() , themeId);
         return ResponseEntity.status(HttpStatus.CREATED).body(reviewerDto);
     }
 
     @DeleteMapping("/{themeId}/reviewer/{reviewerId}")
     public ResponseEntity deleteReviewer(@PathVariable long themeId ,
                                          @PathVariable long reviewerId) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         userService.deleteReviewerFromTheme(themeId , reviewerId);
+        log.info(
+                "Admin(vkId={}) удалил ревьювера (reviewerId={}) из (Theme={}))",
+                user.getVkId() , reviewerId , themeId);
         return ResponseEntity.noContent().build();
     }
 }
