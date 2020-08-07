fb32ae2471860bb5ccb958c67e245f8cff658c54
Admin <mkuzivanov@gmail.com>
2020-08-07 15:50:46 +0300

    Подправил поля классов Fixed- и FreeThemeDto

--- src/main/java/spring/app/dto/FixedThemeDto.java
+++ src/main/java/spring/app/dto/FixedThemeDto.java
@@ -7,6 +7,13 @@ public class FixedThemeDto  extends ThemeDto {
     @NotBlank
     private final String type = "fixed";
 
+    public FixedThemeDto() {
+    }
+
+    public FixedThemeDto(Long id, String title, Integer criticalWeight, Integer position, Integer reviewPoint) {
+        super(id, title, criticalWeight, position, reviewPoint);
+    }
+
     public String getType() {
         return type;
     }
--- src/main/java/spring/app/dto/FreeThemeDto.java
+++ src/main/java/spring/app/dto/FreeThemeDto.java
@@ -7,6 +7,14 @@ public class FreeThemeDto extends ThemeDto {
     @NotBlank
     private final String type = "free";
 
+    public FreeThemeDto() {
+        super();
+    }
+
+    public FreeThemeDto(Long id, String title, Integer criticalWeight, Integer position, Integer reviewPoint) {
+        super(id, title, criticalWeight, position, reviewPoint);
+    }
+
     public String getType() {
         return type;
     }
