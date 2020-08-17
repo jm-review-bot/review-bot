365aa368f53d26f73fe0167247e9006aa0a1eda4
JeRiF74 <jerif174@gmail.com>
2020-08-17 22:14:23 +0300

    исправил недостаток связанный с добавлением пользователя .

--- src/main/java/spring/app/dto/FixedThemeDto.java
+++ src/main/java/spring/app/dto/FixedThemeDto.java
@@ -1,17 +1,7 @@
 package spring.app.dto;
 
-import spring.app.groups.CreateGroup;
-import spring.app.groups.UpdateGroup;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
 
 public class FixedThemeDto  extends ThemeDto {
-
-    @Null(groups=UpdateGroup.class)
-    @NotNull(groups= CreateGroup.class)
-    private String type;
 
     public FixedThemeDto() {
         super();
@@ -21,7 +11,4 @@ public class FixedThemeDto  extends ThemeDto {
         super(id, title, criticalWeight, position, reviewPoint);
     }
 
-    public String getType() {
-        return type;
-    }
 }
--- src/main/java/spring/app/dto/FreeThemeDto.java
+++ src/main/java/spring/app/dto/FreeThemeDto.java
@@ -1,17 +1,7 @@
 package spring.app.dto;
 
-import spring.app.groups.CreateGroup;
-import spring.app.groups.UpdateGroup;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 
 public class FreeThemeDto extends ThemeDto {
-
-    @Null(groups= UpdateGroup.class)
-    @NotNull(groups= CreateGroup.class)
-    private String type;
 
     public FreeThemeDto() {
         super();
@@ -21,7 +11,4 @@ public class FreeThemeDto extends ThemeDto {
         super(id, title, criticalWeight, position, reviewPoint);
     }
 
-    public String getType() {
-        return type;
-    }
 }
