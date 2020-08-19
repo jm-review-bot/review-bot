8e403e1ed23d82bd8b1ca013606b955ceadaf0dc
Admin <mkuzivanov@gmail.com>
2020-08-19 23:34:34 +0300

    Перенос инициализации данных в property

--- src/main/java/spring/app/configuration/ApplicationContextConfig.java
+++ src/main/java/spring/app/configuration/ApplicationContextConfig.java
@@ -2,6 +2,7 @@ package spring.app.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,13 +13,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan("spring.app")
 @EnableTransactionManagement
+@PropertySource("classpath:${spring.profiles.general}.properties")
 public class ApplicationContextConfig implements WebMvcConfigurer {
-
-	/*@Bean(initMethod = "init")
-	@Autowired
-	public TestDataInit initTestData(UserService userService, RoleService roleService) {
-		return new TestDataInit(userService, roleService);
-	}*/
 
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/", "classpath:/resources/",
@@ -35,4 +31,6 @@ public class ApplicationContextConfig implements WebMvcConfigurer {
                     CLASSPATH_RESOURCE_LOCATIONS);
         }
     }
+
+
 }
