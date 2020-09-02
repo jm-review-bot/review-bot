2dad85fb002bbb51d52a5524144cc6a2c6aaddaf
Admin <mkuzivanov@gmail.com>
2020-09-02 22:01:21 +0300

    Блокирование фарма RP

--- src/main/java/spring/app/service/abstraction/ReviewStatisticService.java
+++ src/main/java/spring/app/service/abstraction/ReviewStatisticService.java
@@ -1,21 +1,17 @@
 package spring.app.service.abstraction;
 
-import org.springframework.transaction.annotation.Transactional;
 import spring.app.model.ReviewStatistic;
 
 public interface ReviewStatisticService {
 
-    @Transactional
     void updateReviewStatistic(ReviewStatistic reviewStatistic);
+
+    void startReviewStatisticForUser(Integer userVkId);
 
     ReviewStatistic getReviewStatisticByUserVkId(Integer userVkId);
 
     ReviewStatistic getReviewStatisticByUserId(Long userId);
 
-    @Transactional
-    ReviewStatistic getUpdatedStatisticForUser(Integer userVkId);
-
-    @Transactional
     void unblockTakingReviewForUser(Long userId);
 
 }
