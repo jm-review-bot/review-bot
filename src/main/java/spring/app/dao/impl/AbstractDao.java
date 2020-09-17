9499fa11cc00a95e368725405f65347c9d81f1fe
Mihail Klopotnuk <m.klopotnuk@gmail.com>
2020-09-17 23:57:01 +0500

    Abstract getAllByIds method

--- src/main/java/spring/app/dao/impl/AbstractDao.java
+++ src/main/java/spring/app/dao/impl/AbstractDao.java
@@ -5,9 +5,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public abstract class AbstractDao<PK extends Serializable, T> {
 
@@ -21,7 +25,7 @@ public abstract class AbstractDao<PK extends Serializable, T> {
         this.persistentClass = persistentClass;
     }
 
-    @Transactional(propagation= Propagation.MANDATORY)
+    @Transactional(propagation = Propagation.MANDATORY)
     public void save(T entity) {
         entityManager.persist(entity);
     }
@@ -30,12 +34,12 @@ public abstract class AbstractDao<PK extends Serializable, T> {
         return entityManager.find(persistentClass, id);
     }
 
-    @Transactional(propagation= Propagation.MANDATORY)
+    @Transactional(propagation = Propagation.MANDATORY)
     public void update(T entity) {
         entityManager.merge(entity);
     }
 
-    @Transactional(propagation= Propagation.MANDATORY)
+    @Transactional(propagation = Propagation.MANDATORY)
     public void deleteById(PK id) {
         T entity = entityManager.find(persistentClass, id);
         entityManager.remove(entity);
@@ -54,5 +58,19 @@ public abstract class AbstractDao<PK extends Serializable, T> {
         for (T entity : entities) {
             entityManager.remove(entity);
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<T> getAllByIds(List<Long> ids) {
+        String genericClassName = persistentClass.toGenericString();
+        genericClassName = genericClassName.substring(genericClassName.lastIndexOf('.') + 1);
+        List<T> entitys = new ArrayList<>();
+        final String finalGenericClassName = genericClassName;
+        ids.forEach(id -> {
+            Query query = entityManager.createQuery("SELECT e FROM " + finalGenericClassName + " e WHERE e.id = :id");
+            query.setParameter("id", id);
+            entitys.add((T) query.getResultList());
+        });
+        return entitys;
     }
 }
