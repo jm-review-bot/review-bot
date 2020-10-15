14e33becfe4a12f56e81d722c0226eddda3adf99
Mihail Klopotnuk <m.klopotnuk@gmail.com>
2020-10-15 19:26:59 +0500

    Fix Errors

--- src/main/java/spring/app/controller/AdminUserRestController.java
+++ src/main/java/spring/app/controller/AdminUserRestController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 import spring.app.dto.UserDto;
 import spring.app.exceptions.IncorrectVkIdsException;
 import spring.app.groups.CreateGroup;
